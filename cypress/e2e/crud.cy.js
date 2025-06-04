c9e482399e688f9fc4154be0d5dd9aec31440c65
Erick Lance <erickprofissional02@gmail.com>
2025-06-04 15:20:46 -0300

    test: create custom Cypress commands to centralize logic and reduce test duplication
    - add reusable login session command using cy.session to speed up auth flow
    - implement dynamic form submission helper for multiple test scenarios
    - improve test performance and maintainability by reducing repetitive code

--- cypress/e2e/crud.cy.js
+++ cypress/e2e/crud.cy.js
@@ -1,47 +1,22 @@
-
 import { faker } from '@faker-js/faker'
-//faker.setLocale('en')
 
 describe('CRUD', () => {
   it('CRUDs a note', () => {
     const noteDescription = faker.lorem.words(4)
 
     cy.intercept('GET', '**/notes').as('getNotes')
-    cy.intercept('GET', '**/notes/**').as('getNote')
     cy.sessionLogin()
 
-    cy.visit('/notes/new')
-    cy.get('#content').type(noteDescription)
-    cy.contains('button', 'Create').click()
-
-    cy.wait('@getNotes')
-    cy.contains('.list-group-item', noteDescription)
-      .should('be.visible')
-      .click()
-    cy.wait('@getNote')
-
-    const updateNoteDescription = faker.lorem.words(4)
-
-    cy.get('#content')
-      .as('contentField')
-      .clear()
-    cy.get('@contentField')
-      .type(updateNoteDescription)
-    cy.contains('button', 'Save').click()
+    cy.createNote(noteDescription)
     cy.wait('@getNotes')
 
-    cy.contains('.list-group-item', noteDescription).should('not.exist')
-    cy.contains('.list-group-item', updateNoteDescription)
-      .should('be.visible')
-      .click()
-    cy.wait('@getNote')
-    cy.contains('button', 'Delete').click()
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
+
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
     cy.wait('@getNotes')
 
-    cy.get('.list-group-item')
-      .its('length')
-      .should('be.at.least', 1)
-    cy.contains('.list-group-item', updateNoteDescription)
-      .should('not.exist')
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait('@getNotes')
   })
 })
